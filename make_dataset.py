85f3438e9142a129621321f58fedc0a715462118
Wahab <akawafi3@gmail.com>
2020-05-13 11:36:33 +0100

    make dataset

--- make_dataset.py
+++ make_dataset.py
@@ -9,16 +9,15 @@ vidspath = Path('Data/Videos/')
 vidnames = [e for e in vidspath.iterdir()]
 
 for i in vidnames:
-    print(i)
     #print(i.suffix)
     #print(i.stem)
     info = i.stem.split('_')
-    print(info)
     seperator = '/'
     new_path = 'Data/clean/' + seperator.join(info)
     Path(new_path).mkdir(parents=True, exist_ok=True)
     new_vidpath = Path(new_path) / i.name
-    print(new_vidpath)
-    i.replace(new_vidpath)
-
+    if not new_vidpath.exists():
+        with new_vidpath.open(mode='xb') as fid:
+            fid.write(i.read_bytes())
+    #i.replace(new_vidpath)
 
