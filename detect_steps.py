a349b542d0231fe02a970663fabeb80bfc332207
Wahab <wahab@pop-os.localdomain>
2020-07-03 14:54:16 +0100

    detect steps 2

--- detect_steps.py
+++ detect_steps.py
@@ -9,7 +9,6 @@ for i in range(1,2):
 	for condition, condition_data in person.items():
 		for run in condition_data:
 			run['name']
-
 			tfm = run['tfm']
 			traj = run['trajectories']
 			frame = run['frame']
@@ -17,7 +16,6 @@ for i in range(1,2):
 			if tfm is not None:
 				transf_traj = run['transf_traj']
 				warped = run['transf_img']
-
 				#lisbon.draw_points(warped, transf_traj)
 				
 				left = transf_traj[0]
@@ -30,11 +28,6 @@ for i in range(1,2):
 					y_distance.append(yl - yr)
 				
 				x_distance = np.array(x_distance)
-				y_distance = np.array(y_distance)
-
-
-
-
 				step_frame = [] # list of frames where a step is detected
 
 				for i in range(0, x_distance.shape[0]-1):
@@ -44,25 +37,17 @@ for i in range(1,2):
 					if (x1 < 0 and x2 > 0) or (x1 > 0 and x2 < 0):
 						step_frame.append(i)
 						step = True
+				
+				total_step_n = len(step_frame)
 
 				x_axis = np.zeros(250)
 				steplist = [(0,x) for x in step_frame]
-				print(x_distance)
 				plt.plot(x_distance, 'r')
-				plt.plot(y_distance, 'b')
 				plt.plot(x_axis)
-				plt.plot(step_frame, np.zeros(len(step_frame)), 'go')
-
+				plt.plot(step_frame, np.zeros(total_step_n), 'go')
 				#plt.axis([0, 250, -100, 100])
 				plt.xlabel('frame')
 				plt.ylabel('distance (pixels)')
-				plt.title('Histogram of IQ')
+				plt.title('X Distance between left and right foot')
 				plt.show()
-
-				
-
-
-
-
-
 
